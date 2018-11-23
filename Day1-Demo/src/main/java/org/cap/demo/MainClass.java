c2d14f8540bc80499901563dea13e4aadea96af9
MyTraining2018 <captrainingnov2016@gmail.com>
2018-11-23 12:27:20 +0530

    First Commit

---
+++ Day1-Demo/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,15 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello! Good Afternoon");
+
+		greetings();
+	}
+	
+	public static void greetings() {
+		System.out.println("Welcome to GIT!");
+	}
+
+}
